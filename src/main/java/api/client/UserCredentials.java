e9eda16e97359b3a6800167e74c4ab1ab0e48ac4
Roman Buryak <infestberserk@gmail.com>
2024-12-07 15:43:20 +0400

    add branch develop_3

--- src/main/java/api/client/UserCredentials.java
+++ src/main/java/api/client/UserCredentials.java
@@ -14,7 +14,4 @@ public class UserCredentials {
     private String email;
     private String password;
 
-    public static UserCredentials from(User user) {
-        return new UserCredentials(user.getEmail(), user.getPassword());
-    }
 }
